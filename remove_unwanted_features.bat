2b8b603e593c5887e8c798cfa7e07a2a3cfa6f03
Kyle van Niekerk <kylevn185@gmail.com>
2025-04-07 09:35:19 +0200

    Remove execution policy, Add startmenu All apps by default

--- remove_unwanted_features.bat
+++ remove_unwanted_features.bat
@@ -10,13 +10,21 @@ if %errorLevel% == 0 (
 )
 
 :: Set execution policy to RemoteSigned for the current user
-powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
-echo Execution policy set to RemoteSigned.
+::powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+::echo Execution policy set to RemoteSigned.
 
+:: Disable Bing search in the Start Menu
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
 echo Registry keys added successfully, Bing search results will no longer appear in the start menu.
 
+:: Enable full right-click context menu
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 echo Full right-click context menu enabled.
+
+:: Configure Start Menu to show all apps by default and hide pinned/recommended apps
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_ShowAllApps /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackProgs /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackDocs /t REG_DWORD /d 0 /f
+echo Start Menu configured to show all apps by default and hide pinned/recommended apps.
 
 pause
