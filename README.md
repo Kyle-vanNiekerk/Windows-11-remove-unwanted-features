1dcb0cf37cf87c0edd2b7fc41c9ee13ed783cae1
Kyle van Niekerk <kylevn185@gmail.com>
2026-05-06 10:55:03 +0200

    Add features to disable Update Readme

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Windows-11-remove-unwanted-features
-This project will do the following when executed:
-* Remove Bing search results from the start menu
-* Remove the new shortened right-click context menu, replacing it with the full context menu
-* Change keyboard layout to US, so typing quotes (",') works normally
-* Change Powershell's execution policy to allow running of node.js scripts
+
+This project provides a lightweight batch script to make Windows 11 feel more like Windows 7 or 10, without aggressive debloating. The script applies safe tweaks such as:
+
+* Remove Bing search results from the Start menu
+* Restore the full (classic) right-click context menu
+* Disable Widgets
+* Remove Taskbar Chat
+* Move the taskbar to the left (classic position)
+* Show all system tray icons
+* Show labels on taskbar buttons
+
+## Customization
+
+Each tweak in the script can be commented or uncommented depending on your requirements. Simply open `remove_unwanted_features.bat` in a text editor and add or remove `::` at the beginning of lines to enable or disable specific features.
+
+No aggressive debloating or removal of core Windows features is performed. The script is intended to provide a familiar, classic Windows experience with minimal risk.
--- remove_unwanted_features.bat
+++ remove_unwanted_features.bat
@@ -9,7 +9,7 @@ if %errorLevel% == 0 (
     exit /b
 )
 
-:: Set execution policy to RemoteSigned for the current user
+:: Set execution policy to RemoteSigned for the current user (for running JavaScript using Node.js or PowerShell scripts if needed)
 ::powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 ::echo Execution policy set to RemoteSigned.
 
@@ -17,8 +17,29 @@ if %errorLevel% == 0 (
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
 echo Registry keys added successfully, Bing search results will no longer appear in the start menu.
 
-:: Enable full right-click context menu
+
+:: Enable full right-click context menu (classic)
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 echo Full right-click context menu enabled.
+
+:: Disable Widgets
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f
+echo Widgets disabled.
+
+:: Remove Taskbar Chat
+:: reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarMn /t REG_DWORD /d 0 /f
+:: echo Taskbar Chat removed.
+
+:: Move taskbar to the left (classic position)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f
+echo Taskbar moved to the left.
+
+:: Show all system tray icons
+:: reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v EnableAutoTray /t REG_DWORD /d 0 /f
+:: echo All system tray icons will be shown.
+
+:: Show labels on taskbar buttons
+:: reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarGlomLevel /t REG_DWORD /d 1 /f
+:: echo Taskbar button labels enabled.
 
 pause
